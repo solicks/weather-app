e8e681d5455cf0e629abaf773c8abe2ee0ab75a8
solicks <solicks8355@gmail.com>
2020-05-21 12:17:57 +0100

    deploy my react app to gitbub pages

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            <hr />
+            <p>Solicks Technology &copy;2020</p>
+        </div>
+    )
+}
+
+export default Footer;
