3e9bb109d52894f52f0318223f5e7d721c945815
solicks <solicks8355@gmail.com>
2020-05-24 11:32:00 +0100

    Updates

---
+++ precache-manifest.3bff19e75e72853eceffcb634166ea8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac2ad7f8ab2b14512bcd2e238f95535",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "aceb2071379e7451ea68",
+    "url": "/weather-app/static/css/main.c856b31c.chunk.css"
+  },
+  {
+    "revision": "119f8b326d08107942e3",
+    "url": "/weather-app/static/js/2.c831ed82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.c831ed82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aceb2071379e7451ea68",
+    "url": "/weather-app/static/js/main.c8241f73.chunk.js"
+  },
+  {
+    "revision": "b583aba1370634b5a5a2",
+    "url": "/weather-app/static/js/runtime-main.60cbc992.js"
+  },
+  {
+    "revision": "60fdbfd3e65b347a98027060e5f8d0d1",
+    "url": "/weather-app/static/media/cold-mountain.60fdbfd3.jpeg"
+  },
+  {
+    "revision": "3b9052550afc9b48928a21d89090a379",
+    "url": "/weather-app/static/media/warm-mountain.3b905255.jpeg"
+  }
+]);
